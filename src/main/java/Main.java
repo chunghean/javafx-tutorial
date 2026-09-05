510683a76d6a9bb30b3f29f11d51aa6d85a6fa44
Chung Hean <Chean1503@gmail.com>
2026-09-05 13:31:02 +0800

    Add title for app window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Duke Chatbot");
 
             stage.setMinHeight(620);
             stage.setMinWidth(417);
